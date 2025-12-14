8607240c1a536322bd20f7f0e622041b3aa74c3a
iaritina <iaritina7@gmail.com>
2025-12-14 19:47:35 +0300

    setup database

---
+++ back-office/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace back_office.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
