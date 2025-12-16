aa88d3fa03a039ab631435ed74e732142cacc802
iaritina <iaritina7@gmail.com>
2025-12-16 18:52:27 +0300

    Doctor's Entity CRUD

--- back-office/Models/Doctor.cs
+++ back-office/Models/Doctor.cs
@@ -1,18 +1,25 @@
-﻿
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace back_office.Models
 {
+    [Table("DOCTORS")]
     public class Doctor
     {
-        [Key]
-        public int Id { get; set; }
-        public string RegistrationNumber { get; set; }
+        [Key] [Column("ID_DOC")] public int Id { get; set; }
+
         [Required]
-        public string FirstName { get; set; }
+        [Column("DOC_NAME")]
+        [StringLength(80)]
+        public string? FullName { get; set; }
+
+        [Column("EMAIL")] [StringLength(255)] public string? Email { get; set; }
+
         [Required]
-        public string LastName { get; set; }
-        public string? Email { get; set; }
+        [Column("CONTACT")]
+        [StringLength(80)]
+        public string? Contact { get; set; }
+
+        [Column("IS_DISABLED")] public int? IsDisabled { get; set; } = 0;
     }
 }
--- back-office/Program.cs
+++ back-office/Program.cs
@@ -1,4 +1,5 @@
 using back_office.Data;
+using back_office.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<DoctorService>();
 
 var app = builder.Build();
 
