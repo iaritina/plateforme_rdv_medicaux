e747bf19720e06b60e244ad3282e13701e9cebf1
Haroavo <razakamahefaharoavo9@gmail.com>
2025-12-16 16:00:08 +0300

    [patient] CRUD Patient #feature/patient

---
+++ back-office/Controllers/PatientController.cs
@@ -0,0 +1,119 @@
+﻿using back_office.Data;
+using back_office.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace back_office.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+     
+        public PatientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10)
+        {
+            var totalPatients = await _context.Patients.CountAsync();
+            var patients = await _context.Patients
+                .OrderBy(p => p.Id)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            ViewBag.CurrentPage = pageNumber;
+            ViewBag.TotalPages = (int)Math.Ceiling(totalPatients / (double)pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalPatients = totalPatients;
+
+            return View(patients);
+        }
+
+
+        
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var patient = await _context.Patients.FirstOrDefaultAsync(p => p.Id == id);
+            if (patient == null) return NotFound();
+
+            return View(patient);
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(patient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null) return NotFound();
+
+            return View(patient);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Patient patient)
+        {
+            if (id != patient.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(patient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Patients.Any(e => e.Id == id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(patient);
+        }
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var patient = await _context.Patients.FirstOrDefaultAsync(p => p.Id == id);
+            if (patient == null) return NotFound();
+
+            return View(patient);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            _context.Patients.Remove(patient);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
