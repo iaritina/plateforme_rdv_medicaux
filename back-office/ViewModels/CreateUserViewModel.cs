86d7bae225f7113a027931e78c7134c100bf2f33
Haroavo <razakamahefaharoavo9@gmail.com>
2025-12-23 19:38:19 +0300

    [faeture/refactorization] modifying register form

--- back-office/ViewModels/CreateUserViewModel.cs
+++ back-office/ViewModels/CreateUserViewModel.cs
@@ -4,11 +4,28 @@ namespace back_office.ViewModels;
 
 public class CreateUserViewModel
 {
-    [Required] [EmailAddress] public string Email { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [Display(Name = "Nom d'utilisateur")]
+    public string UserName { get; set; }
+
+    [Display(Name = "Téléphone")]
+    [Phone]
+    public string PhoneNumber { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
+    [Display(Name = "Mot de passe")]
     public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirmer le mot de passe")]
+    [Compare("Password", ErrorMessage = "Le mot de passe et la confirmation ne correspondent pas.")]
+    public string ConfirmPassword { get; set; }
 
     public string? Role { get; set; } // "Patient", "Doctor", "Admin"
 }
